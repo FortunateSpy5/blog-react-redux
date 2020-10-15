e6422047b5327f073737699d56ff049dfdec29b0
Soumotanu Mazumdar <msoumotanu@gmail.com>
2020-10-15 17:19:33 +0530

    Completed

--- src/reducers/postsReducer.js
+++ src/reducers/postsReducer.js
@@ -1,6 +1,6 @@
-export default (posts = [], action) => {
-  if (action.type === 'FETCH_POSTS') {
-    return action.payload;
-  }
-  return posts;
+export default (state = [], action) => {
+    if (action.type === 'FETCH_POSTS') {
+        return action.payload;
+    }
+    return state;
 };
